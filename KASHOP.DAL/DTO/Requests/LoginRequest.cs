3b4f63de64a77265e67e32339c50eeb6c4664a2b
sh_khade <s.soboh1@student.aaup.edu>
2025-10-30 20:17:26 +0200

    feat(auth): add LoginRequest, JwtTokenService, AuthController and appsettings.json

---
+++ KASHOP.DAL/DTO/Requests/LoginRequest.cs
@@ -0,0 +1,8 @@
+namespace KASHOP.DAL.DTO.Requests
+{
+    public class LoginRequest
+    {
+        public string UserNameOrEmail { get; set; }
+        public string Password { get; set; }
+    }
+}
